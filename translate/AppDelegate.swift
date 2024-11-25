20e1d0d2316541be43ccfe132b812d1530800135
RII798 <52328667+RII798@users.noreply.github.com>
2024-11-25 10:10:21 +0300

    AppDelegate.swift

--- translate/AppDelegate.swift
+++ translate/AppDelegate.swift
@@ -3,6 +3,7 @@
 //  translate
 //
 //  Created by Minan on 15.01.2023.
+//  Edited by RII798 on 25.11.2024.
 //
 
 import Cocoa
@@ -25,28 +26,44 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     
     var statusBar: NSStatusBar!
     var statusBarItem: NSStatusItem!
+    var menu: NSMenu!
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         panel = FloatingPanel()
         
         statusBar = NSStatusBar()
         statusBarItem = statusBar.statusItem(withLength: NSStatusItem.variableLength)
-
+        
         if let button = statusBarItem.button {
             button.image = NSImage(named: "icon")
             button.action = #selector(statusBarItemPressed)
         }
         
-        let key = Constants.key
-        let modifiers = Constants.modifiers
+       
         
-        // Setup hot key for control+space
-        hotKey = HotKey(key: key, modifiers: modifiers)
+        //Hotkey to open panel without interacting with status menu icon
+        hotKey = HotKey(key: .t, modifiers: [.command, .control])
+        hotKey?.keyDownHandler = {
+            self.panel.toggle()
+        }
+        
+        // Status Bar menu
+        menu = NSMenu()
+        menu.addItem(NSMenuItem(title: "Quit", action: #selector(quitApp), keyEquivalent: "q"))
+        
+        NSEvent.addLocalMonitorForEvents(matching: [.rightMouseDown]) { [weak self] event in
+            self?.handleRightClick(event: event)
+            return event
+        }
     }
-
-    func applicationWillTerminate(_ aNotification: Notification) {
-        // Insert code here to tear down your application
-    }
+    
+    
+    //If Right-click, open menu
+    func handleRightClick(event: NSEvent) {
+        if let button = statusBarItem.button {
+            menu.popUp(positioning: nil, at: NSPoint(x: 0, y: button.bounds.height), in: statusBarItem.button)
+        }
+     }
 
     func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
         return true
@@ -54,6 +71,10 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     
     @objc func statusBarItemPressed() {
         self.panel.toggle()
+    }
+    
+    @objc func quitApp() {
+        NSApplication.shared.terminate(self)
     }
 }
 
